767a471551fef155f5b30fd391e2f368d5193995
Augusto Corvalan <augustocorvalan@gmail.com>
2018-06-01 22:09:39 -0400

    add rectangle and polygon element types w/ examples

--- Elements.js
+++ Elements.js
@@ -20,9 +20,53 @@ class EllipseElement extends Renderable {
             startX = 0,
             startY = 0,
             width = 10,
-            height
+            height = 10
         } = this.state;
 
         ellipse(startX, startY, width, height)
     }
 }
+
+class RectElement extends Renderable {
+    render(newState) {
+        this.state = {...this.state, ...newState};
+
+        const {
+            startX = 0,
+            startY = 0,
+            width = 10,
+            height = 10
+        } = this.state;
+
+        rect(startX, startY, width, height)
+    }
+}
+
+class PolygonElement extends Renderable {
+    render(newState) {
+        this.state = {...this.state, ...newState};
+
+        const {
+            startX = 0,
+            startY = 0,
+            radius = 20,
+            sides = 6,
+            rotation = 0
+        } = this.state;
+
+        push();
+        translate(startX, startY);
+        rotate(rotation)
+        const rotAngle = 360 / sides;
+
+        beginShape();
+        for (let i = 0; i < sides; i++) {
+            const thisVertex = pointOnCircle(0, 0, radius / 2, i * rotAngle)
+            vertex(thisVertex.x, thisVertex.y)
+        }
+        endShape(CLOSE)
+
+        pop()
+    }
+}
+
--- utils.js
+++ utils.js
@@ -1,3 +1,9 @@
 function randomNum(min, max = 1) {
     return min + Math.floor(Math.random() * Math.floor(max));
 }
+
+const pointOnCircle = (posX, posY, radius, angle) => {
+    const x = posX + radius * cos(angle)
+    const y = posY + radius * sin(angle)
+    return createVector(x, y)
+}
